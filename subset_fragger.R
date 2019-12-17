4d2b43ab6769360d5153cb1d2fb3c2634f6c2f2d
Brett Phinney <bsphinney@ucdavis.edu>
2019-12-16 17:20:49 -0800

    small changes

--- subset_fragger.R
+++ subset_fragger.R
@@ -1,18 +1,19 @@
-# subest feasta
+# create a subest fasta from Fragpipe/MSFragger results and initial database
 
 library('tidyverse')
 library("seqinr")
 
+# setwd to your dir. Should contain the .fas database and the fragpipe/MSfragger
+# protein.tsv output file
+setwd("E:\\brett\\Data\\HELA_QC")
 
-setwd("E:\\brett\\UNIVERSITY_OF_GEORGIA\\HELA_QC")
-
-# read fasta
+# read fasta used in the MSFragger search
 fasta <- read.fasta("2019-12-16-td-rev-UP000005640.fas", seqtype = "AA",as.string = TRUE, set.attributes = FALSE)
 
-# read fragger results
+# read fragpipe/msfragger protein results file
 fragger <- read_tsv("protein.tsv")
 
-# subset fasta to only include fragger results
+# create subset fasta that only includes MSFragger results (usually the FDR is 1%)
 fragger.subset<- names(fasta) %in% fragger$Protein
 fragger.subset.fasta <- fasta[fragger.subset]
 
