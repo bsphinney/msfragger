064e709a60d1d18bef064a3b941b957a9d6b5e20
Brett Phinney <bsphinney@ucdavis.edu>
2019-12-16 17:45:12 -0800

    small comment changes

--- subset_fragger.R
+++ subset_fragger.R
@@ -7,7 +7,7 @@ library("seqinr")
 # protein.tsv output file
 setwd("E:\\brett\\Data\\HELA_QC")
 
-# read fasta used in the MSFragger search
+# read fasta used in the MSFragger search change this to the one use used 
 fasta <- read.fasta("2019-12-16-td-rev-UP000005640.fas", seqtype = "AA",as.string = TRUE, set.attributes = FALSE)
 
 # read fragpipe/msfragger protein results file
